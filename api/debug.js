c9bb708744ea7031d0dacc4b789a23d738aa50f6
garrybuilds <290832292+garrybuilds@users.noreply.github.com>
2026-07-23 16:49:30 +0000

    chore: remove debug endpoint

--- api/debug.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    has_key: !!process.env.RESEND_API_KEY,
-    key_prefix: process.env.RESEND_API_KEY ? process.env.RESEND_API_KEY.substring(0, 6) + '...' : 'MISSING'
-  });
-};
